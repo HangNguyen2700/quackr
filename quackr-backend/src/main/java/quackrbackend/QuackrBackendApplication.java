6c7a8adb697a09a4b6a406491d5c6fd1f8b3bac5
HangNguyen2700 <laniday2711@gmail.com>
2023-06-03 20:21:41 +0700

    init quackr backend project

---
+++ quackr-backend/src/main/java/quackrbackend/QuackrBackendApplication.java
@@ -0,0 +1,13 @@
+package quackrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuackrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuackrBackendApplication.class, args);
+    }
+
+}
